75603d2a998df331b026c57792b9c519cb4ec31a
jonathan de alba <JonathanDeAlba@jonathaacintosh.lan>
2023-03-27 22:50:00 -0700

    first commit

---
+++ pushPop.js
@@ -0,0 +1,6 @@
+let firstArray = [1, 2, 3, 4, 5];
+let itemRemovedFromFirstArray = firstArray.pop();
+//add item to this array
+let secondArray = [];
+secondArray.push(itemRemovedFromFirstArray);
+console.log(secondArray);
